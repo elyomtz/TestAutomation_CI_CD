e6ba50bea049aa1cbb96008e576c84601c9b1610
Elyoenai Mtz <elyo.mr@gmail.com>
2026-09-02 20:26:05 -0600

    modifying yml file and environment variables

--- Core/Configuration/ConfigurationManager.cs
+++ Core/Configuration/ConfigurationManager.cs
@@ -1,23 +1,21 @@
 ﻿using Microsoft.Extensions.Configuration;
 
-
 namespace TestAutomation_CI_CD.Core.Configuration
 {
     public static class ConfigurationManager
     {
         public static TestSettings Settings { get; }
-
+ 
         static ConfigurationManager()
         {
             var configuration = new ConfigurationBuilder()
                 .SetBasePath(AppContext.BaseDirectory)
                 .AddJsonFile("appsettings.json", optional: false)
+                .AddEnvironmentVariables()
                 .Build();
 
             Settings = configuration.Get<TestSettings>()
-                       ?? throw new InvalidOperationException(
-                           "Could not load test settings.");
+                       ?? throw new InvalidOperationException("Could not load test settings.");
         }
-
     }
 }
